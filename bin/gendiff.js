2c3ff8c2a432784b666a9739ce3e02c7e82ff90c
ivekhov <ivekhov@gmail.com>
2022-10-29 22:39:54 +0300

    add arguments to cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('filepath1')
+  .argument('filepath2')
   .version('output the version number');
 
 program.parse();
