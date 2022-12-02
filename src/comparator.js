0037ddba151a6849ca0c9a2e0cdda590239d05c6
ivekhov <ivekhov@gmail.com>
2022-12-03 02:55:52 +0300

    update formattersDev

--- src/comparator.js
+++ src/comparator.js
@@ -1,31 +1,30 @@
 import _ from 'lodash';
 
-
 const compare = (objectOld, objectNew) => {
   const keys = _.union(Object.keys(objectOld), Object.keys(objectNew));
 
-  return keys.map((key) =>  {
-    if ( (Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key) ) && (typeof objectNew[key] === 'object' && typeof objectOld[key] === 'object')) {
+  return keys.map((key) => {
+    if ((Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key)) && (typeof objectNew[key] === 'object' && typeof objectOld[key] === 'object')) {
       return {
         node: key,
         status: 'nested',
         value: compare(objectOld[key], objectNew[key]),
       };
     }
-    if ( (Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key) ) && (objectNew[key] === objectOld[key])) {
+    if ((Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key)) && (objectNew[key] === objectOld[key])) {
       return {
         node: key,
         status: 'unchanged',
         value: objectOld[key],
       }
     }
-    if ( (Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key) ) && (objectNew[key] !== objectOld[key])) {
+    if ((Object.hasOwn(objectNew, key) && Object.hasOwn(objectOld, key)) && (objectNew[key] !== objectOld[key])) {
       if (Object.hasOwn(objectNew, key)) {
-          return {
-            node: key,
-            status: 'updated',
-            value: [objectNew[key], objectOld[key]], //'deleted',
-          };
+        return {
+          node: key,
+          status: 'updated',
+          value: [objectNew[key], objectOld[key]],
+        };
       };
     }
     if (Object.hasOwn(objectNew, key) && !Object.hasOwn(objectOld, key)) {
--- src/formattersDev.js
+++ src/formattersDev.js
@@ -1,31 +1,46 @@
+// 1 walk through objects in list
+// 2 create formatter per object with recursive
+
 const stylish = (items) => {
-  const obj = {};
+  const EMPTY = '  ';
+  const PLUS = '+ ';
+  const MINUS = '- ';
+  const TAB = '  ';
+  let mainResult = '';
+
+  // Function #1 for object formatting
+  const crawler = (item, accumulator, depth) => {
+    switch (item.status) {
+      case 'deleted':
+        return `${TAB.repeat(depth)}${MINUS}${item.node}: ${item.value}\n`;
+      case 'added':
+        return `${TAB.repeat(depth)}${PLUS}${item.node}: ${item.value}\n`;
+      case 'unchanged':
+        return `${TAB.repeat(depth)}${EMPTY}${item.node}: ${item.value}\n`;
+      case 'updated':
+        return `${TAB.repeat(depth)}${MINUS}${item.node}: ${item.value[1]}\n${TAB.repeat(depth)}${PLUS}${item.node}: ${item.value[0]}\n`;
+      case 'nested':
+        // ToDo: walk through list if nested
+        accumulator += `${TAB.repeat(depth)}${EMPTY}${item.node}: {\n`;
+
+        for (const itemInternal of item.value) {
+          accumulator += `${TAB.repeat(depth)}${crawler(
+            itemInternal,
+            '',
+            depth + 1
+          )}${TAB.repeat(depth - 1)}`;
+        }
+        accumulator += `${TAB.repeat(depth)}${EMPTY}}\n`;
+    }
+    accumulator += `${TAB.repeat(depth - 1)}${EMPTY}}\n`;
+    return accumulator;
+  };
+
+  // ToDo: Function #2 - walk through items in list
   for (const item of items) {
-
-    if (item.status === 'nested') {
-      obj[`  ${item.node}\n`] = stylish(item.value);
-    } else if (item.status === 'unchanged') {
-      obj[`  ${item.node}`] = item.value;
-    } else if (item.status === 'deleted') {
-      obj[`- ${item.node}`] = item.value;
-    } else if (item.status === 'added') {
-      obj[`+ ${item.node}`] = item.value;
-    } else if (item.status === 'updated') {
-      obj[`- ${item.node}`] = item.value[1];
-      obj[`+ ${item.node}`] = item.value[0];
-    } 
+    mainResult += `${crawler(item, '', 1)}`;
   }
-  // return obj;
-
-  let diff = JSON.stringify(obj, undefined, 4);
-  diff = JSON.stringify(obj, '"', '');
-
-  diff = diff.replaceAll('\\n', '');
-  diff = diff.replaceAll('\\', '');
-  diff = diff.replaceAll('"', '');
-  diff = diff.replaceAll(',', '');
-  return diff;
-
+  return mainResult;
 };
 
 export { stylish };
