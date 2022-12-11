fed415b02adb574beedae05ff45128c1c52b0175
ivekhov <ivekhov@gmail.com>
2022-12-11 22:41:44 +0300

    implement formatters

--- src/formatters.js
+++ src/formatters.js
@@ -1,14 +1,70 @@
-import * as yaml from 'js-yaml';
+import _  from 'lodash';
 
-const stylish = (obj) => {
-  let diff;
 
-  // ToDo: space char '\ ' on linter error message
-  diff = JSON.stringify(obj, null, '\  ');
-
-  diff = diff.replaceAll('"', '');
-  diff = diff.replaceAll(',', '');
-  return diff;
+const sortTree = (tree) => {
+  if (tree.status === 'nested') {
+    return sortTree(tree.value)
+  }
+  const myOrderedTree = _.sortBy(tree, o => o.node);
+  return myOrderedTree;
 };
+
+
+const stringify = (currentValue, replacer, depth) => {
+
+  if (typeof currentValue !== 'object' || currentValue === null) {
+    return `${currentValue}`;
+  }
+
+  const indentSize = depth;
+  const currentIndent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - 1); 
+
+  const lines = Object
+    .entries(currentValue)
+    .map(([key, val]) => 
+      `${currentIndent}${key}: ` + 
+      `${stringify(val, replacer, depth + 1)}`
+    );
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
+};
+
+const stylish = (diffTree) => {
+  const TAB = '    ';
+
+  const crawler = (list, depth) => {
+    const currentIndent = TAB.repeat(depth );
+    const bracketIndent = TAB.repeat(depth - 1);
+
+    const sortedList = sortTree(list);
+
+    const lines = sortedList
+      .map((currentItem) => {
+        switch (currentItem.status) {
+          case 'added':
+            return `${currentIndent.slice(0, -2)}+ ${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;
+          case 'deleted':
+            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;         
+          case 'updated':
+            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: ${stringify(currentItem.valueOld, TAB, depth + 1)}\n${currentIndent.slice(0, -2)}+ ${currentItem.node}: ${stringify(currentItem.valueNew, TAB, depth + 1)}`;
+          case 'unchanged':
+            return `${currentIndent}${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;
+          case 'nested':
+            return `${currentIndent}${currentItem.node}: ${crawler(currentItem.value, depth + 1)}`;            
+        }
+      });
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  }
+  
+  return crawler(diffTree, 1);
+}
 
 export { stylish };
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,65 +1,11 @@
-import _ from 'lodash';
 import fileParser from './parsers.js';
+import { compare } from './comparator.js';
 import { stylish } from './formatters.js';
 
-export default (pathFirstFile, pathSecondFile, outputFormatter = stylish) => {
-  const objectFirst = fileParser(pathFirstFile);
-  const objectSecond = fileParser(pathSecondFile);
-
-  const entriesSecond = Object.entries(objectSecond);
-  const entriesFirst = Object.entries(objectFirst);
-
-  const keysAll = {
-    common: [],
-    new: [],
-    deleted: [],
-  };
-
-  const commonItems = {};
-  const newItems = {};
-  const deletedItems = {};
-
-  for (const [key, value] of entriesSecond) {
-    if (Object.hasOwn(objectFirst, key)) {
-      if (value === objectFirst[key]) {
-        keysAll.common.push(key);
-        commonItems[key] = value;
-      } else {
-        keysAll.new.push(key);
-        newItems[key] = value;
-        keysAll.deleted.push(key);
-        deletedItems[key] = objectFirst[key];
-      }
-    } else {
-      keysAll.new.push(key);
-      newItems[key] = value;
-    }
-  }
-
-  for (const [key, value] of entriesFirst) {
-    if (!Object.hasOwn(objectSecond, key)) {
-      keysAll.deleted.push(key);
-      deletedItems[key] = value;
-    }
-  }
-
-  const keysUnique = _.union(keysAll.common, keysAll.deleted, keysAll.new).sort();
-
-  const orderedResult = {};
-
-  for (const key of keysUnique) {
-    if (Object.hasOwn(deletedItems, key)) {
-      orderedResult[`- ${key}`] = deletedItems[key];
-    }
-    if (Object.hasOwn(newItems, key)) {
-      orderedResult[`+ ${key}`] = newItems[key];
-    }
-    if (Object.hasOwn(commonItems, key)) {
-      orderedResult[`  ${key}`] = commonItems[key];
-    }
-  }
-
-  const diff = outputFormatter(orderedResult);
-
-  return diff;
+export default (pathOldFile, pathNewFile, outputFormatter = stylish) => {
+  const objectOld = fileParser(pathOldFile);
+  const objectNew = fileParser(pathNewFile);
+  const diff = compare(objectOld, objectNew);
+  const result = outputFormatter(diff);
+  return result;
 };
