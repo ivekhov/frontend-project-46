ac225668fd9e46be0698aa6731c2f7ac74ce4201
ivekhov <ivekhov@gmail.com>
2022-12-11 20:14:49 +0300

    fix stylish in formatter

--- src/formattersDevScenarioOne.js
+++ src/formattersDevScenarioOne.js
@@ -1,50 +1,46 @@
-const stringify = (value, replacer, spacesCount) => {
+const stringify = (currentValue, replacer = '    ', depth = 1) => {
 
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return `${currentValue}`;
-    }
+  if (typeof currentValue !== 'object' || currentValue === null) {
+    return `${currentValue}`;
+  }
 
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize -1 ); 
+  const indentSize = depth;
+  const currentIndent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - 1); 
 
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => 
-        `${currentIndent}${key}: ` + 
-        `${iter(val, depth + 1)}`
-      );
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };  
-  return iter(value, 1);
+  const lines = Object
+    .entries(currentValue)
+    .map(([key, val]) => 
+      `${currentIndent}${key}: ` + 
+      `${stringify(val, replacer, depth + 1)}`
+    );
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
 };
 
 const stylish = (diffTree) => {
-  const TAB = '____';
+  const TAB = '    ';
+
   const crawler = (list, depth) => {
-    const currentIndent = TAB.repeat(depth);
+    const currentIndent = TAB.repeat(depth );
     const bracketIndent = TAB.repeat(depth - 1);
 
     const lines = list
       .map((currentItem) => {
         switch (currentItem.status) {
-
           case 'added':
-            return `${currentIndent.slice(0, -2)}+ ${currentItem.node}:  added depth is ${depth} ${stringify(currentItem.value, TAB, depth + 1)}`;
-
+            return `${currentIndent.slice(0, -2)}+ ${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;
           case 'deleted':
-            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: deleted  depth is ${depth} ${stringify(currentItem.value, TAB, depth + 1)}`;         
+            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;         
           case 'updated':
-            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: updated-deleted  depth is ${depth} ${stringify(currentItem.valueOld, TAB, depth + 1)}\n${currentIndent.slice(0, -2)}+ ${currentItem.node}: updated-added  depth is ${depth}  ${stringify(currentItem.valueNew, TAB, depth + 1)}`;
+            return `${currentIndent.slice(0, -2)}- ${currentItem.node}: ${stringify(currentItem.valueOld, TAB, depth + 1)}\n${currentIndent.slice(0, -2)}+ ${currentItem.node}: ${stringify(currentItem.valueNew, TAB, depth + 1)}`;
           case 'unchanged':
-            return `${currentIndent}${currentItem.node}: unchanged depth is ${depth}  ${stringify(currentItem.value, TAB, depth + 1)}`;
+            return `${currentIndent}${currentItem.node}: ${stringify(currentItem.value, TAB, depth + 1)}`;
           case 'nested':
-            return `${currentIndent}${currentItem.node}: ${crawler(currentItem.value, depth + 1)}`;
+            return `${currentIndent}${currentItem.node}: ${crawler(currentItem.value, depth + 1)}`;            
         }
       });
     return [
@@ -53,12 +49,8 @@ const stylish = (diffTree) => {
       `${bracketIndent}}`,
     ].join('\n');
   }
+  
   return crawler(diffTree, 1);
 }
 
 export { stylish };
-
-
-
-
-/////
