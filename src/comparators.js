ecbb6406c07904003d0e60b06c796c0106b4e159
ivekhov <ivekhov@gmail.com>
2023-01-07 20:57:26 +0300

    add minor changes for linter

--- src/comparators.js
+++ src/comparators.js
@@ -2,47 +2,27 @@ import _ from 'lodash';
 
 const compareObjects = (objectOld, objectNew) => {
   const keys = _.union(Object.keys(objectOld), Object.keys(objectNew));
-
   return keys.map((key) => {
     if (!Object.hasOwn(objectOld, key)) {
-      return {
-        node: key,
-        status: 'added',
-        value: objectNew[key],
-      };
+      return { node: key, status: 'added', value: objectNew[key] };
     }
     if (!Object.hasOwn(objectNew, key)) {
-      return {
-        node: key,
-        status: 'deleted',
-        value: objectOld[key],
-      };
+      return { node: key, status: 'deleted', value: objectOld[key] };
     }
     if (_.isObject(objectNew[key]) && _.isObject(objectOld[key])) {
-      return {
-        node: key,
-        status: 'nested',
-        value: compareObjects(objectOld[key], objectNew[key]),
-      };
+      return { node: key, status: 'nested', value: compareObjects(objectOld[key], objectNew[key]) };
     }
     if (objectNew[key] === objectOld[key]) {
-      return {
-        node: key,
-        status: 'unchanged',
-        value: objectOld[key],
-      };
+      return { node: key, status: 'unchanged', value: objectOld[key] };
     }
     if (objectNew[key] !== objectOld[key]) {
       if (Object.hasOwn(objectNew, key)) {
         return {
-          node: key,
-          status: 'updated',
-          valueOld: objectOld[key],
-          valueNew: objectNew[key],
+          node: key, status: 'updated', valueOld: objectOld[key], valueNew: objectNew[key],
         };
       }
     }
-    throw new Error(`Objects are not comparable. Check input.${objectOld} \n ${objectNew}`);
+    throw new Error(`Objects are not comparable. Check input. ${objectOld} \n ${objectNew}`);
   });
 };
 
